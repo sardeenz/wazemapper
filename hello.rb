348368179df993fcb24b37cd1c413340154a8d0f
Chad Foley <chad.foley@raleighnc.gov>
2015-11-04 17:04:04 -0500

    restructured hello.rb to be more DRY and looping through results now to isolate values

--- hello.rb
+++ hello.rb
@@ -1,17 +1,32 @@
 require 'sinatra'
+require 'rubygems'
 require 'httparty'
+
+class CoREsriAPI
+
+  include HTTParty
+  base_uri 'http://maps.raleighnc.gov/arcgis/rest/services'
+  default_params :output => 'json'
+  format :json
+
+  def self.getSpecialEventData(precision)
+  	@display = []
+    @eventdata = get('/SpecialEvents/SpecialEventsView/MapServer/0/query?where=1%3D1&outSR=4326&geometryPrecision=6&f=json')
+  		 @eventdata['features'].each do |object|
+		   	puts object['attributes']
+		    @display << object['attributes']
+		 end		
+		 return @display
+  end
+
+end
+
 
 get '/' do
   "Hello World!"
 end
 
 get '/getEventData' do
-	response = HTTParty.get('http://maps.raleighnc.gov/arcgis/rest/services/SpecialEvents/SpecialEventsView/MapServer/0/query?where=1%3D1&outSR=4326&f=json')
-	#puts response.body, response.code, response.message, response.headers.inspect
-
-	# convert to waze schema
-
-	return response.body
+	 CoREsriAPI.getSpecialEventData('6').to_json
 end
-
 
